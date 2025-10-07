3789b6208a1044552a6c57aafd06b3ae35bc7801
Rana Mansi <ranamansim1@gmail.com>
2025-10-07 16:32:08 +0530

    delete all files

--- p1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int a=30;
-int b=6;
-printf("%d*%d=%d",a,b,a*b);
-}
-
--- p2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int a=50;
-int b=8;
-printf("%dmod%d=%d",a,b,a%b);
-}
